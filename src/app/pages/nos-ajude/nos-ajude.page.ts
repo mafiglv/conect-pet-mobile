dcefdeaaad35da1f41e8479613a289b3b0b25835
Germária Lins <germarialins@hotmail.com>
2024-11-28 17:32:21 -0300

    novos ajustes

--- src/app/pages/nos-ajude/nos-ajude.page.ts
+++ src/app/pages/nos-ajude/nos-ajude.page.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nos-ajude',
   templateUrl: './nos-ajude.page.html',
   styleUrls: ['./nos-ajude.page.scss'],
 })
-export class NosAjudePage implements OnInit {
+export class NosAjudePage {
+  showMore = false;
 
-  constructor() { }
+  // Lista de doações adicionais
+  moreDonations = [
+    { image: 'assets/images/cobertores.png', title: 'Cobertores', description: 'Cobertores para aquecer', price: 'R$20,00' },
+    { image: 'assets/images/casinhas.png', title: 'Casinhas', description: 'Abrigos para animais', price: 'R$150,00 - R$300,00' },
+    { image: 'assets/images/brinquedos.png', title: 'Brinquedos', description: 'Brinquedos para cães e gatos', price: 'R$10,00 - R$50,00' },
+  ];
 
-  ngOnInit() {
+  // Função para alternar entre mostrar mais ou menos
+  toggleDonations() {
+    this.showMore = !this.showMore;
   }
-
 }

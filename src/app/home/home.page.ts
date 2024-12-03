88030d5dc8ee7e605a13091d494ae132fe21ac1f
Germária Lins <germarialins@hotmail.com>
2024-12-03 16:36:00 -0300

    mudanças em home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
-  constructor(private router: Router) {}
-
-  ngOnInit() {
-    // Aguarde 3 segundos antes de redirecionar para a próxima página
-    setTimeout(() => {
-      this.router.navigateByUrl('/adote-com-amor'); // Altera para a próxima tela
-    }, 3000); // 3000 milissegundos = 3 segundos
-  }
+export class HomePage {
+  constructor() {}
 }
